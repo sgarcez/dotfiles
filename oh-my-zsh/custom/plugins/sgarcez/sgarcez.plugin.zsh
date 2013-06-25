b5a9cf34b2d55f193284e801f79969bb88fa6a29
Sergio Garcez <garcez.sergio@gmail.com>
2013-06-25 17:13:01 +0100

    added puppet vim syntaxh + default editor

--- oh-my-zsh/custom/plugins/sgarcez/sgarcez.plugin.zsh
+++ oh-my-zsh/custom/plugins/sgarcez/sgarcez.plugin.zsh
@@ -1,6 +1,9 @@
 
-alias e='subl . &'
+#alias e='subl . &'
 
 # add plugin's bin directory to path
 # export PATH="$(dirname $0)/bin:$PATH"
-# export EDITOR='subl'
+export EDITOR='vim'
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
