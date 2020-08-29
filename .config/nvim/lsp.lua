5ca4b4ca8c02619c0afc662a19240fce761ff0f4
Sergio Garcez <garcez.sergio@gmail.com>
2020-08-29 12:12:27 +0100

    LSP Rust

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -34,31 +34,18 @@ nvim_lsp.gopls.setup{
     }
 }
 
-nvim_lsp.rust_analyzer.setup{}
-
-nvim_lsp.pyls.setup{
-    cmd = { "/home/sgarcez/pyls-venv/bin/pyls" }
+nvim_lsp.rust_analyzer.setup{
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            completion = {
+                addCallArgumentSnippets = false,
+                addCallParenthesis = false,
+            }
+        }
+    }
 }
 
+nvim_lsp.pyls.setup{}
+
 require'nvim_lsp'.intelephense.setup{}
-
--- -- organize imports sync
--- function go_org_imports(options, timeout_ms)
---   local context = { source = { organizeImports = true } }
---   vim.validate { context = { context, 't', true } }
---   local params = vim.lsp.util.make_range_params()
---   params.context = context
-
---   local results = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
---   print(vim.inspect(result))
---   if not result then return end
---   vim.lsp.util.apply_text_edits(result[1].result)
-
---   -- local params = vim.lsp.util.make_formatting_params(options)
---   -- local result = vim.lsp.buf_request_sync(0, "textDocument/formatting", params, timeout_ms)
---   -- if not result then return end
---   -- result = result[1].result
---   -- vim.lsp.util.apply_text_edits(result)
--- end
-
--- vim.api.nvim_command("au BufWritePre *.go lua go_org_imports({}, 1000)")
