b807271b1b15001158a767add7dfa8c8f55cd2b1
Sergio Garcez <garcez.sergio@gmail.com>
2025-02-15 20:03:34 +0200

    wezterm macos

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,4 +14,7 @@ config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
 
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
+
 return config
